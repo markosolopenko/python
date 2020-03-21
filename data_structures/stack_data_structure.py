1c57eb2c50454235b5c5fb0577d83368d6027d97
marko <kauchuk1616@gmail.com>
2020-03-21 12:24:45 +0200

    Day-16

--- data_structures/stack_data_structure.py
+++ data_structures/stack_data_structure.py
@@ -55,18 +55,35 @@ class Stack:
         :return:
         """
         count = 0
-        for a in self.data:
+        for _ in self.data:
             count += 1
         return count
 
+    def is_balanced(self, expression):
+        exist_braces = {'{': '}', '[': ']', '(': ')'}
+        queue = []
+        for brace in expression:
+            if brace in exist_braces.keys():
+                queue.append(exist_braces[brace])
+            elif brace in exist_braces.values():
+                if not queue or brace != queue.pop():
+                    return False
+        return not queue
+
+
+
 
 my_stack = Stack()
-my_stack.push(12)
-print(my_stack.peek())
-print(my_stack.is_empty())
-my_stack.push(48)
-print(my_stack.peek())
-print(my_stack.peek())
-print(my_stack.length())
-print(my_stack.is_empty())
-print(my_stack.data)
+# my_stack.push(12)
+# print(my_stack.peek())
+# print(my_stack.is_empty())
+# my_stack.push(48)
+# print(my_stack.peek())
+# print(my_stack.peek())
+# print(my_stack.length())
+# print(my_stack.is_empty())
+# print(my_stack.data)
+assert my_stack.is_balanced('(()()())') is True
+assert my_stack.is_balanced('())') is False
+assert my_stack.is_balanced('[](){({}{[]})}') is True
+assert my_stack.is_balanced('[{]}') is False
